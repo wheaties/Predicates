46e9a8908b1a4cd1bc0781d3b937b3756cdf3187
Owein Reese <owreese@gmail.com>
2012-12-08 06:12:58 -0800

    closer to immutability, almost a State Monad

--- src/main/scala/com/wheaties/choice/iteration/Accept.scala
+++ src/main/scala/com/wheaties/choice/iteration/Accept.scala
@@ -2,52 +2,40 @@ package com.wheaties.choice.iteration
 
 object AcceptAll extends IterationScheme{
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = true
-
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = this
 }
 
 object AcceptNone extends IterationScheme{
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = false
-
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = false
 }
 
-//TODO: think about how to lose all this mutable state but still remain GC friendly!
 class AcceptEvery(n: Int) extends IterationScheme{
-
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = count % n == 0
-
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = this
 }
 
-class AcceptEveryF(f: Int => Int, init: Int) extends IterationScheme{
+class AcceptEveryF(f: Int => Int, init: Int) extends ReplacingScheme{
   require(init > 0)
 
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = count % init == 0
 
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = if(accept(value, count)) new AcceptEveryF[A](f, f(init))
+  def replace = new AcceptEveryF(f, f(init))
 }
 
 class AcceptFirst(n: Int) extends IterationScheme{
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = count < n - 1
-
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = this
 }
 
 class AcceptIf[@specialized(Int, Long, Float, Double) -A](pred: A => Boolean) extends IterationScheme{
   def accept[B <: A](value: B, count: Int) = pred(value)
-
-  def next[B <: A](value: B, count: Int) = this
 }
 
-class AcceptUntil[@specialized(Int, Long, Float, Double) -A](pred: A => Boolean) extends IterationScheme{
-  def accept[B <: A](value: A, count: Int) = pred(value)
-
-  def next[B <: A](value: B, count: Int) = if(accept(value, count)) AcceptNone else this
-}
-
-class AcceptOnce[@specialized(Int, Long, Float, Double) -A](pred: A => Boolean) extends IterationScheme{
+class AcceptUntil[@specialized(Int, Long, Float, Double) -A](pred: A => Boolean) extends ReplacingScheme{
   def accept[B <: A](value: B, count: Int) = pred(value)
 
-  def next[B <: A](value: B, count: Int) = if(accept(value, count)) AcceptAll else this
+  def replace = AcceptNone
+}
+
+class AcceptOnce[@specialized(Int, Long, Float, Double) -A](pred: A => Boolean) extends ReplacingScheme{
+  def accept[B <: A](value: B, count: Int) = pred(value)
+
+  def replace = AcceptAll
 }
--- src/main/scala/com/wheaties/choice/iteration/IterationScheme.scala
+++ src/main/scala/com/wheaties/choice/iteration/IterationScheme.scala
@@ -8,27 +8,13 @@ trait IterationScheme extends PredicateLike[IterationScheme]{
 
   def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int): Boolean
 
-  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int): IterationScheme
-
-  def compose(that: IterationScheme): IterationScheme = new IterationScheme {
-    def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
-      (that accept (value, count)) && (self accept (value, count))
-
-    def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = {
-      val selfN = self next (value, count)
-      val thatN = that next (value, count)
-
-      if(selfN == self && thatN == that) this else selfN compose thatN
-    }
-  }
-
-  def andThen(that: IterationScheme): IterationScheme = that compose self
+  def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int): IterationScheme = this
 
   protected[iteration] implicit def neg = new Negation[IterationScheme]{
     def not(that: IterationScheme) = new IterationScheme {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) = !(that accept (value, count))
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
+      override def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
         val thatN = that next (value, count)
 
         if(thatN == that) this else Not(thatN)
@@ -37,76 +23,68 @@ trait IterationScheme extends PredicateLike[IterationScheme]{
   }
 
   protected[iteration] implicit def con = new Connective[IterationScheme,IterationScheme,IterationScheme]{
-    def and(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def and(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         (p accept (value, count)) && (q accept (value, count))
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext and qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext and qNext
     }
 
-    def or(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def or(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         (p accept (value, count)) || (q accept (value, count))
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext or qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext or qNext
     }
 
-    def xor(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def xor(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         if(p accept (value, count)) !(q accept (value, count)) else q accept (value, count)
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext xor qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext xor qNext
     }
 
-    def nand(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def nand(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         if(p accept (value, count)) false else !(q accept (value, count))
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext nand qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext nand qNext
     }
 
-    def nor(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def nor(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         if(p accept (value, count)) !(q accept (value, count)) else q accept (value, count)
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext nor qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext nor qNext
     }
 
-    def nxor(p: IterationScheme, q: IterationScheme) = new IterationScheme {
+    def nxor(p: IterationScheme, q: IterationScheme) = new MultiScheme(p, q) {
       def accept[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
         if(p accept (value, count)) q accept (value, count) else !(q accept (value, count))
 
-      def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
-        val pNext = p next (value, count)
-        val qNext = q next (value, count)
-
-        if(pNext == p && qNext == q) this else pNext nxor qNext
-      }
+      def replace(pNext: IterationScheme, qNext: IterationScheme) = pNext nxor qNext
     }
   }
+}
+
+abstract class ReplacingScheme extends IterationScheme{
+  override def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) =
+    if(accept(value, count)) replace else this
+
+  protected[iteration] def replace: IterationScheme
+}
+
+abstract class MultiScheme(p: IterationScheme, q: IterationScheme) extends IterationScheme
+    with Product2[IterationScheme, IterationScheme]{
+  def _1 = p
+  def _2 = q
+
+  override def next[@specialized(Int, Long, Float, Double) A](value: A, count: Int) ={
+    val pNext = p next (value, count)
+    val qNext = q next (value, count)
+
+    if(pNext == p && qNext == q) this else replace(pNext, qNext)
+  }
+
+  protected[iteration] def replace(pNext: IterationScheme, qNext: IterationScheme): IterationScheme
 }

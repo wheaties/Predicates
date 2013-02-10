092e40969a35cac150a059abe16421e6842a950e
Owein Reese <owreese@gmail.com>
2013-02-10 12:12:36 -0500

    required Set[Elem], found Set[Elem]... um, wtf?

--- src/main/scala/com/wheaties/choice/Choice.scala
+++ src/main/scala/com/wheaties/choice/Choice.scala
@@ -9,22 +9,22 @@ trait Choice[-Value]{
   protected[choice] def scheme: Accept[Value]
   protected[choice] def iteration(iter: Accept[Value]): Accept[Value]
 
-  def get[C](collection: C)(implicit prune: Prune[Value,C]) = prune(collection, iteration(scheme))
+  def get[C](collection: C)(implicit traverse: Traverse[Value,C]) = traverse.prune(collection, iteration(scheme))
 
   def set[A,C](collection: C, value: A)(implicit traverse: Traverse[Value,C], replace: Replace[Value,A]) =
-    traverse.onTrue(replace)(collection,iteration(scheme))
+    traverse.replace(collection, replace, iteration(scheme))
 
-  //TODO: this needs to handle List[_], Map[_,_] and Map[Key,_] so that we can have our cake and eat it too?
+  //TODO: this needs to handle List[_], Map[_,_] and Map[Key,_] so that we can have our cake and eat it too.
   def mod[C](collection: C, f: Value => Value)(implicit traverse: Traverse[Value,C]) =
-    traverse.onTrue(f)(collection,iteration(scheme))
+    traverse.replace(collection, f, iteration(scheme))
 
-  def partition[C](collection: C)(implicit divide: Partition[Value,C]) = divide(collection, iteration(scheme))
+  def partition[C](collection: C)(implicit traverse: Traverse[Value,C]) = traverse.part(collection, iteration(scheme))
 
-  //def fold[A,B,C](collection: C, init: Value, f: A)(implicit fold: Fold[Value,C,A]) =
-  //  fold(collection, init, f, iteration(scheme))
+  //def fold[A,B,C](collection: C, init: Value, f: A)(implicit traverse: Traverse[Value,C]) =
+  //  traverse.fold(collection, init, f, iteration(scheme))
 
-  //def reduce[A](collection: C, f: A)(implicit fold: Fold[Value,C,A]) =
-  //  fold(collection, f, iteration(scheme))
+  //def reduce[A](collection: C, f: A)(implicit traverse: Traverse[Value,C]) =
+  //  traverse.reduce(collection, f, iteration(scheme))
 
   def compose[V <: Value](that: Choice[V]) = that andThen this
 
@@ -34,7 +34,7 @@ trait Choice[-Value]{
   }
 }
 
-object Choice extends PruneImplicits with TraverseImplicits with PartitionImplicits{
+object Choice extends TraverseImplicits{
   implicit def neg[Value] = new Negation[Choice[Value]] {
     def not(that: Choice[Value]) = new Choice[Value] {
       protected[choice] def scheme = Not(that scheme)
--- src/main/scala/com/wheaties/choice/iteration/Traverse.scala
+++ src/main/scala/com/wheaties/choice/iteration/Traverse.scala
@@ -1,56 +1,108 @@
 package com.wheaties.choice.iteration
 
-trait Traverse[Elem,Collection] extends ((Collection,Accept[Elem]) => Collection){
+import scala.collection.mutable.{Builder, ArrayBuffer}
 
-  def onTrue(t: Elem => Elem): Traverse[Elem,Collection]
-  def onFalse(f: Elem => Elem): Traverse[Elem,Collection]
-  def on(f: Elem => Elem, t: Elem => Elem): Traverse[Elem,Collection]
+//TODO: The one problem is Stream! Can't iterate over it. It could be infinite... Need to constraint the type bounds.
 
-  def apply(collection: Collection, accept: Accept[Elem]): Collection
+trait Traverse[Elem,Collection]{
+  def prune(collection: Collection, accept: Accept[Elem]): Collection
+  def replace(collection: Collection, f: Elem => Elem, accept: Accept[Elem]): Collection
+  def part(collection: Collection, accept: Accept[Elem]): (Collection, Collection)
+//  def fold[B](init: B)(collection: Collection, f: (B,Elem) => Elem, accept: Accept[Elem]): B
+//  def reduce[B](collection: Collection, f: (B,Elem) => Elem, accept: Accept[Elem]): B
 }
 
-trait TraverseImplicits{
-  implicit def traverseIter[Elem,C[Elem] <: Iterable[Elem]] = TraverseIterable()
-  implicit def traverseArray[Elem,A[Elem] <: Array[Elem]] = TraverseIterable()
+trait TraverseIterable[Elem,Collection <: Iterable[Elem]] extends Traverse[Elem,Collection]{
 
-  case class TraverseIterable[Elem,C[Elem] <: Iterable[Elem]](t: Elem => Elem = identity[Elem] _,
-                                                              f: Elem => Elem = identity[Elem] _)
-      extends Traverse[Elem,C[Elem]]{
-    def onTrue(func: Elem => Elem) = copy(t = func)
-    def onFalse(func: Elem => Elem) = copy(f = func)
-    def on(fFunc: Elem => Elem, tFunc: Elem => Elem) = copy(t = tFunc, f = fFunc)
+  def prune(collection: Collection, accept: Accept[Elem]) ={
+    val acc = builder
+    def onNext(next: Elem){ if(accept accept(next)) acc += t(next) }
+    traverse(collection, onNext)
 
-    def apply(collection: C[Elem], accept: Accept[Elem]) ={
-      val acc = collection.companion.newBuilder[Elem]
-      val iter = collection.iterator
-      while(iter hasNext){
-        val next = iter next ()
-        if(accept accept(next)) acc += t(next)
-        else acc += f(next)
-      }
+    acc result ()
+  }
 
-      acc result ()
+  def replace(collection: Collection, f: Elem => Elem, accept: Accept[Elem]) ={
+    val acc = builder
+    def onNext(next: Elem){ if(accept accept(next)) acc += f(next) else acc += next }
+    traverse(collection, onNext)
+
+    acc result ()
+  }
+
+  def part(collection: Collection, accept: Accept[Elem]) ={
+    val accTrue = builder
+    val accFalse = builder
+    def onNext(next: Elem){ if(accept accept(next)) accTrue += next else accFalse += next }
+
+    traverse(collection, onNext)
+
+    (accTrue result (), accFalse result ())
+  }
+
+//    def fold[B](init: B)(collection: Collection, f: (B,Elem) => B, accept: Accept[Elem]) ={
+//      var current = init
+//      def onTrue(elem: Elem){ current = f(current, elem) }
+//
+//      current
+//    }
+//
+//    @tailrec final def reduce[B](collection: Collection, f: (B,Elem) => Elem, accept: Accept[Elem]) ={
+//      if(accept accept (collection.head)) fold(collection.head)(collection.tail, f, accept)
+//      else reduce(collection.tail, f, accept)
+//    }
+
+  protected def traverse(collection: Collection, action: Elem => Unit) ={
+    val iter = collection.iterator
+    while(iter hasNext){
+      action(iter next ())
     }
   }
 
-  case class TraverseArray[Elem : ClassManifest,
-                           A[Elem] <: Array[Elem]](t: Elem => Elem = identity[Elem] _,
-                                                   f: Elem => Elem = identity[Elem] _)
-    extends Traverse[Elem,A[Elem]]{
-    def onTrue(func: Elem => Elem) = copy(t = func)
-    def onFalse(func: Elem => Elem) = copy(f = func)
-    def on(fFunc: Elem => Elem, tFunc: Elem => Elem) = copy(t = tFunc, f = fFunc)
+  protected def builder: Builder[Elem,Collection]
+}
 
-    def apply(collection: A[Elem], accept: Accept[Elem]) ={
-      val acc = collection.companion.newBuilder[Elem]
-      val iter = collection.iterator
-      while(iter hasNext){
-        val next = iter next ()
-        if(accept accept(next)) acc += t(next)
-        else acc += f(next)
-      }
+trait TraverseImplicits{
+  implicit def list[Elem,List[Elem]]: Traverse[Elem,List[Elem]] = new TraverseIterable[Elem,List[Elem]] {
+    protected def builder = List.newBuilder[Elem]
+  }
+
+  implicit def set[Elem,Set[Elem]]: Traverse[Elem,Set[Elem]] = new TraverseIterable[Elem,Set[Elem]] {
+    protected def builder = Set.newBuilder[Elem]
+  }
+
+  implicit def traverseArray[Elem : ClassManifest,Array[Elem]] = new Traverse[Elem,Array[Elem]]{
+    def prune(collection: Array[Elem], accept: Accept[Elem]) ={
+      val acc = Array.newBuilder[Elem]
+      def onNext(next: Elem){ if(accept accept(next)) acc += t(next) }
+      traverse(collection, onNext)
 
       acc result ()
+    }
+
+    def replace(collection: Array[Elem], f: Elem => Elem, accept: Accept[Elem]) ={
+      val acc = Array.newBuilder[Elem]
+      def onNext(next: Elem){ if(accept accept(next)) acc += f(next) else acc += next }
+      traverse(collection, onNext)
+
+      acc result ()
+    }
+
+    def part(collection: Array[Elem], accept: Accept[Elem]) ={
+      val accTrue = Array.newBuilder[Elem]
+      val accFalse = Array.newBuilder[Elem]
+      def onNext(next: Elem){ if(accept accept(next)) accTrue += next else accFalse += next }
+      traverse(collection, onNext)
+
+      (accTrue result (), accFalse result ())
+    }
+
+    protected def traverse(collection: Array[Elem], action: Elem => Unit) ={
+      var index = 0
+      val count = collection.length
+      while(index < count){
+        action(collection(index))
+      }
     }
   }
 }
